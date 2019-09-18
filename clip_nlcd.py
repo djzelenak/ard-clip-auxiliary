480924e0375994b8cc08dfd0520537b348ef6192
danzelenak-usgs <dzelenak@contractor.usgs.gov>
2019-09-18 08:44:48 -0500

    allow processing of specific column/row ranges

--- clip_nlcd.py
+++ clip_nlcd.py
@@ -5,9 +5,8 @@ Last Updated: 1/10/2017, 2/2/2017, 9/18/2017 by Dan Zelenak
 """
 
 import datetime
-import glob
 import os
-import pprint
+import datetime as dt
 import subprocess
 import sys
 import argparse
@@ -24,8 +23,33 @@ CONUS_EXTENT = GeoExtent(x_min=-2565585,
                          x_max=2384415,
                          y_max=3314805)
 
+
+now = dt.datetime.now()
+
 t1 = datetime.datetime.now()
 print(t1.strftime("%Y-%m-%d %H:%M:%S"))
+
+
+def make_filename(hv, aux, out_dir):
+    """
+
+    :param aux:
+    :param hv:
+    :param out_file:
+    :return:
+    """
+    name_pattern = "AUX_CU_HHHVVV_20010101_CURRENT-DATE_V01_AUX-NAME.tif"
+
+    replace = {"HHHVVV": f"0{hv[0]}0{hv[1]}",
+               "CURRENT-DATE": now.strftime("%Y%m%d"),
+               "AUX-NAME": aux.upper()}
+
+    out_file = name_pattern
+
+    for key in replace.keys():
+        out_file = out_file.replace(key, replace[key])
+
+    return os.path.join(out_dir, out_file)
 
 
 def geospatial_hv(h, v, loc=CONUS_EXTENT):
@@ -46,44 +70,81 @@ def geospatial_hv(h, v, loc=CONUS_EXTENT):
     return GeoExtent(x_min=xmin, x_max=xmax, y_max=ymax, y_min=ymin)
 
 
-def main():
+def get_hv_list(h_list=(0, 32), v_list=(0, 21)):
+    h_range = range(h_list[0], h_list[1] + 1)
+    v_range = range(v_list[0], v_list[1] + 1)
+
+    out_hv = list()
+
+    for h in h_range:
+        for v in v_range:
+            if len(str(h)) == 1:
+                h = "0" + str(h)
+            else:
+                h = str(h)
+            if len(str(v)) == 1:
+                v = "0" + str(v)
+            else:
+                v = str(v)
+
+            out_hv.append((h, v))
+
+    return out_hv
+
+
+def cli():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-i", "--input", type=str, required=True,
-                        help="Full path to the directory containing the ancillary data products")
+    parser.add_argument("-i", "--input", type=str, required=True, metavar='FILE_PATH',
+                        help="Full path to the input dataset")
 
-    parser.add_argument("-o", "--output", type=str, required=True,
-                        help="Full path to the output location")
+    parser.add_argument("-o", "--output", type=str, required=True, metavar='DIR_PATH',
+                        help="Full path to the output directory")
 
-    parser.add_argument('-hv', nargs=2, type=str, required=False, metavar=('HH (0-32)', 'VV (0-21)'),
-                        help='Horizontal and vertical ARD grid identifiers.')
+    parser.add_argument("--name", type=str, metavar="DATASET_NAME", choices=('NLCD', 'NLCDTRN'),
+                        help="Specify the source dataset name (NLCD or NLCDTRN)")
+
+    parser.add_argument('--hv', nargs=2, type=int, required=False,
+                        metavar=('HH (0-32)', 'VV (0-21)'), default=None,
+                        help='Specify a single tile to process')
+
+    parser.add_argument('--row', nargs=2, type=int, required=False,
+                        metavar=('HH (0-21)'), default=(0, 21),
+                        help='Horizontal grid range to process - must use 2 values - if a single row '
+                             'then use the same value twice')
+
+    parser.add_argument('--col', nargs=2, type=int, required=False,
+                        metavar=('VV (0-32)'), default=(0, 32),
+                        help='Vertical grid range to process - must use 2 values - if a single column '
+                             'then use the same value twice')
 
     args = parser.parse_args()
 
-    file_list = sorted(glob.glob(args.input + os.sep + '*.img'))
+    main(**vars(args))
 
-    if len(file_list) == 0:
+    return None
 
-        file_list = sorted(glob.glob(args.input + os.sep + '*.tif'))
 
-    if len(file_list) == 0:
+def main(input, output, name, hv=None, row=None, col=None):
 
-        print("Couldn't find any .img or .tif files in the specified input folder")
-
+    if (hv and row) or (hv and col):
+        print("Invalid args: a single HV tile was specified along with a row and/or column range")
         sys.exit(0)
 
-    print('Input Ref File List:\n')
+    if not (hv):
+        # Specific row/column range(s)
+        tiles = get_hv_list(h_list=row, v_list=col)
+    else:
+        # A single tile
+        tiles = get_hv_list(h_list=(hv[0], hv[0]), v_list=(hv[1], hv[1]))
 
-    pprint.pprint(file_list)
+    print('\tWorking on {}'.format(os.path.basename(input)))
 
-    for file in file_list:
+    if not os.path.exists(output):
+        os.makedirs(output)
 
-        print('\tWorking on {}'.format(os.path.basename(file)))
-
-        if not os.path.exists(args.output):
-            os.makedirs(args.output)
-
-        clip_extent = geospatial_hv(h=int(args.hv[0]), v=int(args.hv[1]))
+    for tile in tiles:
+        clip_extent = geospatial_hv(h=int(tile[0]), v=int(tile[1]))
 
         print('\n------------------------')
 
@@ -92,9 +153,8 @@ def main():
 
         print('------------------------\n')
 
-        in_file = os.path.basename(file)
-        out_name = os.path.splitext(in_file)[0]
-        out_file = args.output + os.sep + out_name + ".tif"
+        in_file = os.path.basename(input)
+        out_file = make_filename(hv=tile, aux=name, out_dir=output)
 
         print('\tProducing output {}'.format(out_file))
 
@@ -102,7 +162,7 @@ def main():
             ulx=clip_extent.x_min, uly=clip_extent.y_max,
             lrx=clip_extent.x_max, lry=clip_extent.y_min,
             wkt=WKT,
-            src=file,
+            src=input,
             dst=out_file)
 
         subprocess.call(run_trans, shell=True)
@@ -111,7 +171,7 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    cli()
 
 t2 = datetime.datetime.now()
 
